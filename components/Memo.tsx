8c98d6b5cc7c8959916425aa65c7e6d18c2d33f7
lky <skyvip2633@gmail.com>
2023-11-06 19:36:09 +0900

    [feat] 메모창 생성 및 css 수정

--- components/Memo.tsx
+++ components/Memo.tsx
@@ -4,6 +4,7 @@ export default function Memo() {
   return (
     <Container>
       <Title>MEMO</Title>
+      <MemoBox></MemoBox>
     </Container>
   );
 }
@@ -12,11 +13,26 @@ const Container = styled.div`
   width: 100%;
   height: 30vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   padding: 1rem;
   background-color: ${(props) => props.theme.colors.lightgray};
 `;
 
 const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-weight: ${(props) => props.theme.weight.bold};
+  margin-bottom: 0.5rem;
+`;
+
+const MemoBox = styled.textarea`
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  background-color: ${(props) => props.theme.colors.lightgray};
+  border: 0;
+  outline: 0;
+  resize: none;
 `;
