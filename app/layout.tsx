d4607e8a11d78546cc32381db056c22c70a5313c
lky <skyvip2633@gmail.com>
2023-12-30 16:37:35 +0900

    [feat] seo 최적화 meta tag 작성

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,12 @@ import GlobalStyles from "../styles/GlobalStyles";
 
 export const metadata: Metadata = {
   title: "TO TRAVEL IS TO LIVE",
-  description: "new project, to travel is to live!",
+  description:
+    "지역을 검색하여 추천 장소를 알아보고 캘린더 기록 및 메모 작성이 한 페이지 내에서 가능한 여행(국내) 계획 세우기 서비스",
+  keywords: ["Next.js", "국내 여행", "토이 프로젝트"],
+  openGraph: {
+    //추가 학습 후 새로 작성 필요
+  },
 };
 
 export default function RootLayout({
